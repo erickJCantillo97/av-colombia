4d1b7fe083b889abc45ca40865ce7adadae64f9f
Sela soft <selasoft@cotecmar.com>
2024-08-18 21:42:00 -0500

    feat: Update environment configuration for production deployment and enforce HTTPS protocol in AppServiceProvider

--- resources/js/Components/Sections/Menu.vue
+++ resources/js/Components/Sections/Menu.vue
@@ -283,6 +283,29 @@
                     Servicios
                 </Link>
             </li>
+            <div class="flex  justify-end">
+                <div class="justify-end">
+                    <span href="#"
+                        class="text-base text-center  rounded-lg  group transition duration-75 flex items-center p-2 dark:hover:bg-neutral-700 dark:text-neutral-400 dark:hover:text-neutral-300">
+                        <div class="space-x-2 items-center">
+                            <h2 class="font-bold text-sm">{{ $page.props.auth.user.name }}</h2>
+                            <h3 class="text-sm">(Cargo)</h3>
+                        </div>
+                    </span>
+                </div>
+                <ul class="space-y-1.5">
+                    <li>
+                        <Link class="flex items-center gap-x-3.5 py-2 px-2.5 text-sm  rounded-lg dark:hover:bg-neutral-700 dark:text-neutral-400 dark:hover:text-neutral-300"
+                            :href="route('logout')"
+                            method="post"
+                            as="button">
+                            <svg class="text-black w-4 h-4" fill="currentColor" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 512 512" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                <path d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 192 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l210.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128zM160 96c17.7 0 32-14.3 32-32s-14.3-32-32-32L96 32C43 32 0 75 0 128L0 384c0 53 43 96 96 96l64 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32l0-256c0-17.7 14.3-32 32-32l64 0z"/>
+                            </svg>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
         </ul>
     </nav>
 
