28d589efff0d241b093a2bf7b13a96fd0ebe883d
erickJCantillo97 <68025788+erickJCantillo97@users.noreply.github.com>
2025-03-13 06:56:20 -0500

    compilado

--- public/build/assets/PopularSection-B0x5vT_z.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./PopularSection.vue_vue_type_script_setup_true_lang-C2Uq_sUc.js";import"./app-DbNsw2hR.js";export{m as default};
---
+++ public/build/assets/PopularSection-BcleHXtG.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./PopularSection.vue_vue_type_script_setup_true_lang-Bwx5A1dp.js";import"./app-DqKg7NXI.js";export{m as default};
