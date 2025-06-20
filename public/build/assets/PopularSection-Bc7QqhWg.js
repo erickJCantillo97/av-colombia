b274b8f859c21955cb522ba70ed0743e8bd5168b
erick cantillo <erick.cantillo97@gmail.com>
2025-06-20 10:21:02 -0500

    correguido el build

---
+++ public/build/assets/PopularSection-Bc7QqhWg.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./PopularSection.vue_vue_type_script_setup_true_lang-BM5stefc.js";import"./app-DBIBQQ1o.js";export{m as default};
---
+++ public/build/assets/index-BR4SBEKP.js
@@ -0,0 +1,1 @@
+import{e_ as e}from"./app-DBIBQQ1o.js";var a=e();export{a as O};
