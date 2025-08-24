33a7bdd03020935541f21895c325ba3577bcb121
Erick Cantillo <erick.cantillo97@gmail.com>
2025-08-24 17:01:29 -0500

    añadidos servicio de Transporte

--- public/build/assets/PopularSection-BPcfIwfc.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./PopularSection.vue_vue_type_script_setup_true_lang-UsIG3iiV.js";import"./app-CjnSYEpP.js";export{m as default};
---
+++ public/build/assets/index-ZmP_ZXgB.js
@@ -0,0 +1,1 @@
+import{e$ as e}from"./app-VkC6R089.js";var a=e();export{a as O};
