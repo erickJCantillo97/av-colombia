1115120199295654ebcdd61ea08bb1bfb21f2edc
Erick Cantillo <erick.cantillo97@gmail.com>
2025-08-25 12:05:43 -0500

    Add toast notification functionality using SweetAlert2 mixin
    - Implemented a toast notification system with customizable options.
    - Configured toast position, icon color, and appearance settings.
    - Enabled close button and timer for automatic dismissal.

--- public/build/assets/PopularSection-DXPvOtyQ.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./PopularSection.vue_vue_type_script_setup_true_lang-Bc3QnR1s.js";import"./app-CR5TIXgE.js";export{m as default};
---
+++ public/build/assets/index-C7E81G05.js
@@ -0,0 +1,1 @@
+import{e$ as e}from"./app-CevCZ2YM.js";var a=e();export{a as O};
