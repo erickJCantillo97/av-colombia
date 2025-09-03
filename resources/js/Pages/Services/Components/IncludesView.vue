9442712971c320dc3b208a320453a486cc196b13
gienbuja <gienbuja@gmail.com>
2025-09-02 21:15:55 -0500

    refactor(services): improve equipment and includes view components
    - Refactor Equipament.vue to use a features array for consistent rendering
    - Update IncludesView.vue with improved typography and styling

--- resources/js/Pages/Services/Components/IncludesView.vue
+++ resources/js/Pages/Services/Components/IncludesView.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="flex flex-col md:flex-row justify-between items-start gap-x-4 gap-y-6">
         <div class="flex flex-col gap-y-2">
-            <h4 class="text-lg font-semibold text-green-800">Servicios Incluidos</h4>
-            <span class="text-xs italic">Estos son los servicios que se incluyen en tu reserva</span>
+            <h4 class="text-2xl font-semibold text-green-800">Servicios Incluidos</h4>
+            <span class=" italic">Estos son los servicios que se incluyen en tu reserva</span>
             <div v-if="service.type != 'EMBARCACION'">
-                <ul class=" text-gray-700 flex flex-col gap-y-1 text-sm">
+                <ul class=" text-gray-700 flex flex-col gap-y-1 text-md">
                     <li v-for="(include, index) in includes" class="p-1 shadow-sm" :key="index">
                         <i class="fa-solid fa-check text-teal-500"></i>
                         <span class="ml-2 font-semibold">
@@ -14,7 +14,7 @@
                 </ul>
             </div>
             <div v-else>
-                <ul class=" text-gray-700 flex flex-col text-sm">
+                <ul class=" text-gray-700 flex flex-col text-md">
                     <span v-for="(feature, index) in features" :key="index">
                         <li class="mt-2" v-if="includes.includes(feature.name.toUpperCase() || feature.id)">
                             <i class="fa-solid text-teal-500" :class="[feature.icon]"></i>
