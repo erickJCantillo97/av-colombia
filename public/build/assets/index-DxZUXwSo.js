62cef0c7659da94ba40a045518b54c74a2daf29e
Erick Cantillo <erick.cantillo97@gmail.com>
2025-09-03 22:12:10 -0500

    añadido los itinerarios

--- public/build/assets/index-DxZUXwSo.js
+++
@@ -1,1 +0,0 @@
-import{en as l,eo as o,ep as a}from"./app-D3TVovok.js";function c(){a({variableName:o("scrollbar.width").name})}function e(){l({variableName:o("scrollbar.width").name})}export{c as b,e as u};
---
+++ public/build/assets/index-ZDr2Axk-.js
@@ -0,0 +1,1 @@
+import{e_ as e}from"./app-4jSWbsFf.js";var a=e();export{a as O};
