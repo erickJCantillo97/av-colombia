4f7924123d1066194c5fa4e4e021af0b1f471da8
Erick Ccantillo <erick.ccantillo97@gmail.com>
2025-12-04 09:31:33 -0500

    build de las nuevas modificaciones

---
+++ public/build/assets/Portada-BzB8aJNv.js
@@ -0,0 +1,1 @@
+import{f as e,o as s,a as o}from"./app-BjdGmPou.js";const r={class:"flex gap-2 w-full"},t=["src"],n={__name:"Portada",props:{images:Array,portada:String},setup(a){return(c,d)=>(s(),e("div",r,[o("img",{src:a.portada??"/images/productos/baru-1.webp",class:"w-[95vw] h-[30vh] shadow-lg rounded-xl object-cover",alt:"asdasd"},null,8,t)]))}};export{n as default};
--- public/build/assets/index-DxeF40dr.js
+++
@@ -1,1 +0,0 @@
-import{b3 as r}from"./app-h8VW5kjQ.js";var s=r();export{s as O};
