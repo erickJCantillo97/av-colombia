4b652a4cf4df30c903f9111bbfeb5f53f78feda8
Erick Ccantillo <erick.ccantillo97@gmail.com>
2026-05-23 07:16:18 -0500

    fix: corregir nombre de clase de 'Servic' a 'Services' en Service.php

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -11,7 +11,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\Model;
 
-class Servic extends Model
+class Services extends Model
 {
     use Sluggable;
     use HasFactory;
